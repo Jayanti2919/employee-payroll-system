d118aa8c574fdb5d15e3f262433e2bebb4627103
Jayanti2919 <jayanti2919@gmail.com>
2023-07-02 22:08:04 +0530

    set up taolwind and routing

---
+++ client/src/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className='text-5xl font-bold'>
+        Employee Payroll System
+    </div>
+  )
+}
+
+export default Landing
