700e1b87a208f92ce64fd1c3482cbf6c00e5302e
Jayanti2919 <jayanti2919@gmail.com>
2023-07-11 19:14:03 +0530

    Updated company queries

--- server/routes/Company.js
+++ server/routes/Company.js
@@ -2,54 +2,40 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("mysql");
 const connection = require("../utils/Connection.js");
-const bcrypt = require('bcrypt');
-const getDate = require('../utils/GetDate.js');
+const getDate = require("../utils/GetDate.js");
+const Employee = require("../models/employee.model.js");
+const Companies = require("../models/companies.model.js");
 
-router.route('/create').post((req, res) => {
-    const email = req.headers.email;
-    connection.query(`SELECT company_id FROM employee WHERE email='${email}'`, (error, result)=>{
-        if(error) {
-            console.log(error)
-            res.send(JSON.stringify({'message': 'An error occurred 1'}))
-        } else {
-            if(!result[0]){
-                res.send(JSON.stringify({'message': 'Could not create company as you already have a company'}))
-            } else {
-                const data = req.body;
-                const currentDate = getDate();
-                connection.query(`SELECT COUNT(*) AS count FROM companies;`, (err, result) => {
-                    if(err) {
-                        res.send(JSON.stringify({'message': 'An error occurred 2'}))
-                    } else {
-                        let url = null
-                        if(req.body.logo) {
-                            // upload to AWS
-                        }
-                        connection.query(`INSERT INTO companies(id, name, image, email, contact_number, started_on, gstno, status) VALUES (${result[0].count}, '${data.name}', '${url}', '${email}', ${data.contact}, '${currentDate}', '${data.gst}', 'verified')`, (error, result_2)=>{
-                            if(error) {
-                                if(error.errno===1062){
-                                    res.send(JSON.stringify({message:"Company already exists with this email"}))
-                                } else {
-                                    console.log(error)
-                                    res.send(JSON.stringify({message:"Error creating comapany"}))
-                                }
-                            } else {
-                                connection.query(`UPDATE employee SET company_id=${result[0].count} WHERE email='${email}'`, (error2)=>{
-                                    if(error2) {
-                                        console.log(error2)
-                                        res.send(JSON.stringify({message:"Error creating comapany"}))
-                                    } else {
-                                        res.send(JSON.stringify({message:"Company created"}));
-                                    }
-                                })
-                            }
-                        })
-                    }
-                })
-            }
-        }
-    })
-})
+router.route("/create").post(async (req, res) => {
+  const email = req.headers.email;
+  const body = req.body;
 
+  const emp = await Employee.findOne({ where: { email: email } });
+  if (emp.company_id) {
+    res.send(
+      JSON.stringify({
+        message: "Could not create company as you already have a company",
+      })
+    );
+  } else {
+    const currentDate = getDate();
+    try {
+      const company = await Companies.create({
+        name: body.name,
+        email: email,
+        contact_number: body.contact,
+        started_on: currentDate,
+        gstno: body.gst,
+        status: "active",
+    });
+    await company.save();
+    emp.company_id = company.id;
+    res.send(JSON.stringify({message:"Company created"}));
+    } catch (error) {
+      console.log(error);
+      res.send(JSON.stringify({ message: "Error creating comapany" }));
+    }
+  }
+});
 
 module.exports = router;
