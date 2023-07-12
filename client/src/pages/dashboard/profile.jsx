04ed26324a2aadc4eb7735c9404a25bc4d7773b2
Jayanti2919 <jayanti2919@gmail.com>
2023-07-12 09:56:12 +0530

    finished profile

--- client/src/pages/dashboard/profile.jsx
+++ client/src/pages/dashboard/profile.jsx
@@ -1,23 +1,12 @@
 import {
   Card,
   CardBody,
-  CardHeader,
-  CardFooter,
   Avatar,
   Typography,
-  Tabs,
-  TabsHeader,
-  Tab,
   Switch,
   Tooltip,
   Button,
 } from "@material-tailwind/react";
-import {
-  HomeIcon,
-  ChatBubbleLeftEllipsisIcon,
-  Cog6ToothIcon,
-  PencilIcon,
-} from "@heroicons/react/24/solid";
 import { Link, useNavigate } from "react-router-dom";
 import { ProfileInfoCard, MessageCard } from "@/widgets/cards";
 import { useEffect, useState } from "react";
